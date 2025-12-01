27577aefb8fe28560a3d97c888103f9648d05566
Xenophics <sorkdkrak@gmail.com>
2025-12-01 21:52:35 +0900

    d
    d

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
 // =========================================
 function loadData() {
     Promise.all([
-        fetch('/json/data.json').then(res => res.json()),
-        fetch('/json/quests.json').then(res => res.json())
+        fetch('https://wwm.tips/json/data.json').then(res => res.json()),
+        fetch('https://wwm.tips/json/quests.json').then(res => res.json())
     ])
     .then(([mainData, questList]) => {
         globalData = {

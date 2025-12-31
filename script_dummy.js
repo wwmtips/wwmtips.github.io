9a45fd533f0f3878cda24f3b24c1e32b03d80085
Xenophics <sorkdkrak@gmail.com>
2026-01-01 01:04:22 +0900

    ddd

--- script_dummy.js
+++ script_dummy.js
@@ -112,7 +112,6 @@ async function loadMansarokMenu() {
         
         // 1. 만사록 타입 필터링 후 최대 9개만 자르기
         const mansarokList = data
-            .filter(item => item.type === '만사록')
             .slice(0, 6); // [핵심] 0번부터 8번 인덱스까지만 선택
         
         const container = document.getElementById('mansarok-list');

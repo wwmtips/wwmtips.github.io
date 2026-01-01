2ab19520992282554d361dc7e7a510743aca41b9
Xenophics <sorkdkrak@gmail.com>
2026-01-01 10:18:30 +0900

    asd

--- script_dummy.js
+++ script_dummy.js
@@ -881,7 +881,7 @@ const GUIDE_MAP = {
     'news': 'news.html', 'tierlist': 'guide_tier.html', 'weapon': 'tier_weapon.html',
     'build': 'build.html', 'map': 'maps.html', 'side': 'beta.html', 'hw': 'npc.html',
     'boss': 'boss.html', 'marts': 'marts.html', 'harts': 'harts.html', 'skill': 'skils.html',
-    'majang': 'majang.html', 'code': 'code.html', 'moon': 'moon.html', 'b1': 'boss/b1.html'
+    'majang': 'majang.html', 'code': 'code.html', 'moon': 'moon.html', 'b1': 'boss/b1.html', 'b2': 'boss/b2.html', 'b3': 'boss/b3.html'
 };
 
 function loadGuideView() {
